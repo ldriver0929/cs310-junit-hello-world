e0dc1fdb69682e125a84ff54d30f30cf9378b8c4
Logan Driver <ldriver@stu.jsu.edu>
2023-01-29 15:45:47 -0800

    Completed Participation Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -3,21 +3,31 @@ package edu.jsu.mcis.cs310;
 public class Main {
 
     public static void main(String[] args) {
-        
         Main m = new Main();
-        String message = m.getGreeting();
+        
+   
+        
+        String message = m.getGreeting();  
+  
         
         System.out.println(message);
         System.out.println(m.reverse(message));
-        
     }
     
     public String getGreeting() {
         return "Hello, World!";
     }
-    
+
     public String reverse(String message) {
-        return message;
+        String messagereversed = "";
+        int a = message.length() - 1;
+        while (a > -1)
+        {
+        messagereversed += message.charAt(a);  
+        a--;
+        }
+        
+        return messagereversed;
     }
     
 }
